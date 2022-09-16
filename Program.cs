4b7b8a13126ce31d4b3d0fae88b43c0ea9e6e96e
bayrameer <92469944+bayrameer@users.noreply.github.com>
2022-09-16 10:46:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_Cozum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] loto = new int[6];
+
+            Random rastgele = new Random();
+            bool varMi = false;
+
+            for (int i = 0; i < 6; i++)
+            {
+                int sayi = rastgele.Next(1,7);
+                for (int j = 0; j < 6; j++)
+                {
+                    if (sayi == loto[j])
+                    {
+                        varMi = true;
+                        break;
+                    }
+                }
+                if (varMi)
+                {
+                    i--;
+                    varMi = false;
+
+                }
+                else
+                {
+                    loto[i] = sayi;
+                }
+                  
+            }
+            foreach (var item in loto)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
